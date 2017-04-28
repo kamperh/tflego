2472f84890203ee3a74c51eefaa1ebb82efc508d
Herman Kamper <kamperh@gmail.com>
2017-04-28 11:12:13 -0500

    Updated training function with validation

--- tflego/training.py
+++ tflego/training.py
@@ -14,13 +14,11 @@ import timeit
 
 
 def train_fixed_epochs(n_epochs, optimizer, train_loss_tensor,
-        train_feed_iterator, feed_placeholders, test_loss_tensor=None,
-        test_feed_iterator=None, load_model_fn=None, save_model_fn=None,
-        config=None, epoch_offset=0):
+        train_feed_iterator, feed_placeholders, validation_loss_tensor=None,
+        validation_feed_iterator=None, load_model_fn=None, save_model_fn=None,
+        save_best_val_model_fn=None, config=None, epoch_offset=0):
     """
     Train for a fixed number of epochs.
-    
-    TO-DO: Optional validation (and save best model separately).
     
     Parameters
     ----------
@@ -33,11 +31,15 @@ def train_fixed_epochs(n_epochs, optimizer, train_loss_tensor,
         batch.
     feed_placeholders : list of placeholder
         The placeholders that is required for the `train_loss` (and optionally
-        `test_loss`) feeds.
+        `validation_loss`) feeds.
     load_model_fn : str
         If provided, initialize session from this file.
     save_model_fn : str
-        If provided, save session to this file.
+        If provided, save final session to this file.
+    save_best_val_model_fn : str
+        If provided, save the best validation session to this file. If the
+        `validation_loss_tensor` is a list of Tensors, the last value is taken
+        as the current validation loss.
     
     Return
     ------
@@ -45,13 +47,16 @@ def train_fixed_epochs(n_epochs, optimizer, train_loss_tensor,
         Statistics tracked during training. Each key describe the statistic,
         while the value is a list of (epoch, value) tuples.
     """
+
+    assert save_best_val_model_fn is None or validation_loss_tensor is not None
     
     # Statistics
     record_dict = {}
     record_dict["epoch_time"] = []
     record_dict["train_loss"] = []
-    if test_loss_tensor is not None:
-        record_dict["test_loss"] = []
+    if validation_loss_tensor is not None:
+        record_dict["validation_loss"] = []
+        best_validation_loss = np.inf
     
     print datetime.now()
     
@@ -84,17 +89,17 @@ def train_fixed_epochs(n_epochs, optimizer, train_loss_tensor,
             #         )
             #     train_losses.append(cur_loss)
             
-            # # Test model
-            # if test_loss_tensor is not None:
-            #     test_losses = []
-            #     for cur_feed in test_feed_iterator:
+            # # validation model
+            # if validation_loss_tensor is not None:
+            #     validation_losses = []
+            #     for cur_feed in validation_feed_iterator:
             #         cur_loss = session.run(
-            #             [test_loss_tensor],
+            #             [validation_loss_tensor],
             #             feed_dict=feed_dict(cur_feed)
             #             )
-            #         test_losses.append(cur_loss)
-            #     test_loss = np.mean(test_losses)
-            #     record_dict["test_loss"].append((i_epoch, test_loss))
+            #         validation_losses.append(cur_loss)
+            #     validation_loss = np.mean(validation_losses)
+            #     record_dict["validation_loss"].append((i_epoch, validation_loss))
 
             # Train model
             train_losses = []
@@ -118,27 +123,29 @@ def train_fixed_epochs(n_epochs, optimizer, train_loss_tensor,
                 train_loss = np.mean(train_losses, axis=0)
             record_dict["train_loss"].append((i_epoch, train_loss))
 
-            # Test model
-            if test_loss_tensor is not None:
-                test_losses = []
-                if not isinstance(test_loss_tensor, (list, tuple)):
-                    for cur_feed in test_feed_iterator:
+            # Validation model
+            if validation_loss_tensor is not None:
+                validation_losses = []
+                if not isinstance(validation_loss_tensor, (list, tuple)):
+                    for cur_feed in validation_feed_iterator:
                         cur_loss = session.run(
-                            [test_loss_tensor],
+                            [validation_loss_tensor],
                             feed_dict=feed_dict(cur_feed)
                             )
-                        test_losses.append(cur_loss)
-                    test_loss = np.mean(test_losses)
+                        validation_losses.append(cur_loss)
+                    validation_loss = np.mean(validation_losses)
+                    cur_validation_loss = validation_loss
                 else:
-                    for cur_feed in test_feed_iterator:
+                    for cur_feed in validation_feed_iterator:
                         cur_loss = session.run(
-                            test_loss_tensor,
+                            validation_loss_tensor,
                             feed_dict=feed_dict(cur_feed)
                             )
                         cur_loss = np.array(cur_loss)
-                        test_losses.append(cur_loss)
-                    test_loss = np.mean(test_losses, axis=0)
-                record_dict["test_loss"].append((i_epoch, test_loss))
+                        validation_losses.append(cur_loss)
+                    validation_loss = np.mean(validation_losses, axis=0)
+                    cur_validation_loss = validation_loss[-1]
+                record_dict["validation_loss"].append((i_epoch, validation_loss))
 
             # Statistics
             end_time = timeit.default_timer()
@@ -148,9 +155,13 @@ def train_fixed_epochs(n_epochs, optimizer, train_loss_tensor,
             # log = "{:.3f} sec, train loss: {:.5f}".format(epoch_time, train_loss)
             log = "{:.3f} sec".format(epoch_time)
             log += ", train loss: " + str(train_loss)
-            if test_loss is not None:
-                # log += ", test loss: {:.5f}".format(test_loss)
-                log += ", test loss: " + str(test_loss)
+            if validation_loss is not None:
+                # log += ", validation loss: {:.5f}".format(validation_loss)
+                log += ", validation loss: " + str(validation_loss)
+            if save_best_val_model_fn is not None and cur_validation_loss < best_validation_loss:
+                saver.save(session, save_best_val_model_fn)
+                best_validation_loss = cur_validation_loss
+                log += "*"
             print log
             sys.stdout.flush()
 
